bad586e1fc5bc6964bfe671794fe905fbc1268e1
vudat1710 <vudat1710@gmail.com>
2020-05-29 13:50:15 +0700

    delete unused files

--- model/lstm.py
+++
@@ -1,5 +0,0 @@
-from utils.activation import *
-from utils.utils import *
-import pycuda.gpuarray
-from time import time
-
--- tensor.py
+++
@@ -1,7 +0,0 @@
-"""
-Make sure that a tensor is a GPUArray type of pycuda.gpuarray
-"""
-
-import numpy as np
-import pycuda.autoinit
-from pycuda.gpuarray import GPUArray as Tensor
